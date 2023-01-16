bdb3f3f63656c2fca70fea3cf1faaafb6172678c
empty <empty87@yandex.ru>
2023-01-16 23:30:36 +0300

    Prepare resources. Add R.

--- app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -5,7 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.sumin.composition.R
+import com.example.composition.R
+
 
 class ChooseLevelFragment : Fragment() {
 
--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.sumin.composition.R
+import com.example.composition.R
 
 class GameFragment : Fragment() {
 
