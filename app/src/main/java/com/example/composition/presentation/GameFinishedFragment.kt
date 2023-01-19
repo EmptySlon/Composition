736529d343e97a9f87074f8d6127c8c5055101bc
empty <empty87@yandex.ru>
2023-01-19 22:22:13 +0300

    lesson 5

--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -4,29 +4,73 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.composition.databinding.FragmentGameFinishedBinding
+import com.example.composition.domain.entity.GameResult
 
 
 class GameFinishedFragment : Fragment() {
+
+    private lateinit var gameResult: GameResult
 
     private var _binding: FragmentGameFinishedBinding? = null
     private val binding: FragmentGameFinishedBinding
         get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
         return binding.root
-
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    retryGame()
+                }
+
+            })
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        gameResult = requireArguments().getSerializable(GAME_RESULT) as GameResult
+
+    }
+
+    private fun retryGame() {
+        requireActivity().supportFragmentManager.popBackStack(
+            GameFragment.NAME,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
+    }
+
+    companion object {
+        private const val GAME_RESULT = "game Result"
+        fun newInstance(gameResult: GameResult): GameFinishedFragment {
+            return GameFinishedFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(GAME_RESULT, gameResult)
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -5,14 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.composition.R
+import com.example.composition.data.GameRepositoryImpl
 import com.example.composition.databinding.FragmentGameBinding
+import com.example.composition.domain.entity.GameResult
+import com.example.composition.domain.entity.GameSettings
+import com.example.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
+
+    private lateinit var level: Level
+    private lateinit var gameResult: GameResult
+    private lateinit var gameSettings: GameSettings
+    private var winner = false
+    private var countOfRightAnswers = 0
+    private var countOfQuestions = 0
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+        gameSettings = GameRepositoryImpl.getGameSettings(level)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,9 +38,42 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvSum.setOnClickListener{
+            launchGameFinishedFragment()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    private fun launchGameFinishedFragment() {
+
+        gameResult = GameResult(winner, countOfRightAnswers, countOfQuestions, gameSettings )
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object{
+
+        const val KEY_LEVEL = "level"
+        const val NAME = "GameFragment"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 
 
